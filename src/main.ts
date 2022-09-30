b3d924abd281e26b90d7854caa84b5187d3c8482
phan van hieu <phanvanhieu2001.yt@gmail.com>
2022-09-30 22:10:40 +0700

    update 30/9

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { getUserMiddleware } from './util/middleware/get-user.middleware';
 var bodyParser = require('body-parser')
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
