416ccd6fb1968649589727defff9cfb364c85682
phan van hieu <phanvanhieu2001.yt@gmail.com>
2022-10-05 13:32:22 +0700

    2

--- src/util/guard/authentication.guard.ts
+++ src/util/guard/authentication.guard.ts
@@ -2,13 +2,16 @@ import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { ErrorThrowEnum } from "../enum/error.enum";
 
 @Injectable()
-export class AuthenticationGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean {
-    const request = context.switchToHttp().getRequest();
-    const user = request.user;
-    if (!user) {
-      throw new Error(ErrorThrowEnum.UNAUTHORIZED_EXCEPTION);
-    }
-    return true;
-  }
-}
+export class AuthenticationGuard {}
+
+
+// implements CanActivate {
+//   canActivate(context: ExecutionContext): boolean {
+//     const request = context.switchToHttp().getRequest();
+//     const user = request.user;
+//     if (!user) {
+//       throw new Error(ErrorThrowEnum.UNAUTHORIZED_EXCEPTION);
+//     }
+//     return true;
+//   }
+// }
