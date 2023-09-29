e72772056eae8bcf03745cad17294991c31b9c8e
phan van hieu <phanvanhieu2001.yt@gmail.com>
2023-09-29 17:24:43 +0700

    update 29/09/2023

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,68 @@
+// module.exports = {
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     project: 'tsconfig.json',
+//     tsconfigRootDir : __dirname,
+//     sourceType: 'module',
+//   },
+//   plugins: ['@typescript-eslint/eslint-plugin'],
+//   extends: [
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:prettier/recommended',
+//   ],
+//   root: true,
+//   env: {
+//     node: true,
+//     jest: true,
+//   },
+//   ignorePatterns: ['.eslintrc.js'],
+//   rules: {
+//     '@typescript-eslint/interface-name-prefix': 'off',
+//     '@typescript-eslint/explicit-function-return-type': 'off',
+//     '@typescript-eslint/explicit-module-boundary-types': 'off',
+//     '@typescript-eslint/no-explicit-any': 'off',
+//   },
+// };
+
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+  "root": true,
+  "ignorePatterns": ["*/"],
+  "plugins": ["@nx"],
+  "overrides": [
+    {
+      "files": [".ts", ".tsx", ".js", ".jsx"],
+      "rules": {
+        "@nx/enforce-module-boundaries": [
+          "error",
+          {
+            "enforceBuildableLibDependency": true,
+            "allow": [],
+            "depConstraints": [
+              {
+                "sourceTag": "*",
+                "onlyDependOnLibsWithTags": ["*"]
+              }
+            ]
+          }
+        ]
+      }
+    },
+    {
+      "files": [".ts", ".tsx"],
+      "extends": ["plugin:@nx/typescript"],
+      "rules": {}
+    },
+    {
+      "files": [".js", ".jsx"],
+      "extends": ["plugin:@nx/javascript"],
+      "rules": {}
+    },
+    {
+      "files": [".spec.ts", ".spec.tsx", ".spec.js", ".spec.jsx"],
+      "env": {
+        "jest": true
+      },
+      "rules": {}
+    }
+  ]
+}
